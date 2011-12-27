b5d45cd1737082063a131d0b1e6faad03bc872ed
sguimont <sebastieng@sympatico.ca>
2011-12-27 11:38:23 -0500

    Add include tags

--- braindump/grails-app/controllers/HomeController.groovy
+++ braindump/grails-app/controllers/HomeController.groovy
@@ -5,36 +5,46 @@ import com.mushcorp.lt.artefact.TagStatistics
 import com.mushcorp.lt.artefact.Todo
 
 class HomeController {
-	def mongo
-	
+	def springSecurityService
+
 	def index() {
 		flash.error = null
 		flash.warning = null
 		flash.info = null
 
-		def colls = mongo.databaseNames;
-		colls.each {
-			println(it);
+		if(springSecurityService.isLoggedIn()) {
+			render(view:"index", model: [
+						recentNotes: Note.collection.find().sort('dateCreated' : -1).limit(5),
+						recentLinks: Link.collection.find().sort('dateCreated' : -1).limit(5),
+						recentBooks: Book.collection.find().sort('dateCreated' : -1).limit(5),
+						recentTodos: Todo.collection.find().sort('dateCreated' : -1).limit(5)
+					])
 		}
-		
-		render(view:"index", model: [
-					recentNotes: Note.collection.find().sort('dateCreated' : -1).limit(5),
-					recentLinks: Link.collection.find().sort('dateCreated' : -1).limit(5),
-					recentBooks: Book.collection.find().sort('dateCreated' : -1).limit(5),
-					recentTodos: Todo.collection.find().sort('dateCreated' : -1).limit(5)
-		])
+		else {
+			render(view:"index", model: [])
+		}
 	}
 
 	def search() {
-		render(view:"index", model: [
-					recentNotes: Note.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
-					recentLinks: Link.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
-					recentBooks: Book.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
-					recentTodos: Todo.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5)
-		])
+		if(springSecurityService.isLoggedIn()) {
+			render(view:"index", model: [
+						recentNotes: Note.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
+						recentLinks: Link.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
+						recentBooks: Book.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5),
+						recentTodos: Todo.collection.find(["tags" : params.tag]).sort('dateCreated' : -1).limit(5)
+					])
+		}
+		else {
+			render(view:"index", model: [])
+		}
 	}
-	
+
 	def tags() {
-		[tags:TagStatistics.collection.find().sort('value' : -1)]
+		if(springSecurityService.isLoggedIn()) {
+			[tags:TagStatistics.collection.find().sort('value' : -1)]
+		}
+		else {
+			[]
+		}
 	}
 }
--- braindump/grails-app/views/home/tags.gsp
+++ braindump/grails-app/views/home/tags.gsp
@@ -1,6 +1,7 @@
 <!doctype html>
 <html>
 	<head>
+	<sec:ifLoggedIn>
 		<jq:jquery>
 			 var gradient = {
 				 0:    '#f00', // red
@@ -8,22 +9,24 @@
 				 0.66: '#0f0', // green
 				 1:    '#00f'  // blue
 				};
-			 
 			 $("#myCanvas").tagcanvas({weight: true, weightMode:'both', weightFrom: 'data-weight', weightSize: 15.0, weightGradient : gradient},'tags') 
 		</jq:jquery>
+	</sec:ifLoggedIn>
 	</head>
 	<body>
+	<sec:ifLoggedIn>
 		<h3>Tags</h3>
 		<div>
-			<canvas width="300px" height="300px" id="myCanvas">
+			<canvas width="220px" height="220px" id="myCanvas">
 			</canvas>
 			<div id="tags">
 			 	<ul>
 			      <g:each var="tag" in="${tags}">
-					<li><a href="#" style="font-size: <g:formatNumber number="${tag.value.count}" maxFractionDigits="0"/>ex" data-weight="<g:formatNumber number="${tag.value.count}" maxFractionDigits="0"/>">${tag._id}</a></li>
+					<li><a href="<g:createLink controller="home" action="search" params="[tag:tag._id]"/>" style="font-size: <g:formatNumber number="${tag.value.count}" maxFractionDigits="0"/>ex" data-weight="<g:formatNumber number="${tag.value.count}" maxFractionDigits="0"/>">${tag._id}</a></li>
 			      </g:each>
 				</ul>
 			</div>
 		</div>
+	</sec:ifLoggedIn>
 	</body>
 </html>
